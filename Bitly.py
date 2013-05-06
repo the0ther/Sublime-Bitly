41c07e9384fdb9035d01d34987007b2caeedd481
Randy L <randay@gmail.com>
2013-05-06 13:16:48 -0400

    broken wip

--- Bitly.py
+++ Bitly.py
@@ -1,5 +1,18 @@
 import sublime, sublime_plugin
 
-class ExampleCommand(sublime_plugin.TextCommand):
+class BitlyShortenCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		self.view.insert(edit, 0, "Hello, World!")
+		# self.view.insert(edit, 0, "Hello, World!")
+		href = False
+		sels = self.view.sel()
+		for sel in sels:
+			if self.view.substr(sel).find('{') != -1:
+				href = True
+		if not href:
+			new_sels = []
+			for sel in sels:
+				new_sels.append(self.view.find('\}', sel.end()))
+			sels.clear()
+			for sel in new_sels:
+				sels.add(sel)
+			self.view.run_command("expand_selection", { "to": "brackets" })
