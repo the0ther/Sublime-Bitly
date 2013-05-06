df4e4d3f6a93f74c75fea87700567d1e12b07e72
Randy L <randay@gmail.com>
2013-05-06 13:33:36 -0400

    better...

--- Bitly.py
+++ Bitly.py
@@ -1,18 +1,28 @@
-import sublime, sublime_plugin
+import sublime
+import sublime_plugin
+import re
 
 class BitlyShortenCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		# self.view.insert(edit, 0, "Hello, World!")
-		href = False
-		sels = self.view.sel()
-		for sel in sels:
-			if self.view.substr(sel).find('{') != -1:
-				href = True
-		if not href:
-			new_sels = []
-			for sel in sels:
-				new_sels.append(self.view.find('\}', sel.end()))
-			sels.clear()
-			for sel in new_sels:
-				sels.add(sel)
-			self.view.run_command("expand_selection", { "to": "brackets" })
+    def run(self, edit):
+    		# regex search for URLs:
+    		urls = self.view.find_all('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+    		print urls
+
+        # We check for braces since we can do a better job of preserving
+        # whitespace when braces are not present
+        # braces = False
+        # sels = self.view.sel()
+        # for sel in sels:
+        #     if self.view.substr(sel).find('{') != -1:
+        #         braces = True
+
+        # Expand selection to braces, unfortunately this can't use the
+        # built in move_to brackets since that matches parentheses also
+        # if not braces:
+        #     new_sels = []
+        #     for sel in sels:
+        #         new_sels.append(self.view.find('\}', sel.end()))
+        #     sels.clear()
+        #     for sel in new_sels:
+        #         sels.add(sel)
+        #     self.view.run_command("expand_selection", {"to": "brackets"})
---
+++ BitlyApiCall.py
@@ -0,0 +1,26 @@
+class BitlyApiCall(threading.Thread):
+	def __init__(self, sel, string, timeout):
+		self.sel = sel
+		self.original = string
+		self.timeout = timeout
+		self.result = None
+		threading.Thread.__init__(self)
+
+		# the0ther
+		# R_fa589cfdddea41f62a78e21f6e63677f
+	def run(self):
+		try:
+		  data = urllib.urlencode({'longUrl': self.original})
+		  request = urllib2.Request('http://bit.ly/v3/shorten', data,
+		     headers={"User-Agent": "Sublime Bitly"})
+		  http_file = urllib2.urlopen(request, timeout=self.timeout)
+		  self.result = http_file.read()
+		  return
+
+		except (urllib2.HTTPError) as (e):
+		  err = '%s: HTTP error %s contacting API' % (__name__, str(e.code))
+		except (urllib2.URLError) as (e):
+		  err = '%s: URL error %s contacting API' % (__name__, str(e.reason))
+
+		sublime.error_message(err)
+		self.result = False
