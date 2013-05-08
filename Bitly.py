69f85653190e2cb846ce57aa20fe6fc14388e76e
Randy L <randay@gmail.com>
2013-05-08 11:42:32 -0400

    wip compiles

--- Bitly.py
+++ Bitly.py
@@ -17,3 +17,36 @@ class BitlyShortenCommand(sublime_plugin.TextCommand):
 			thread = BitlyApiCall(url, string, 15)
 			threads.append(thread)
 			thread.start()
+
+		self.view.sel().clear()
+
+		edit = self.view.begin_edit('bitly')
+
+		self.handle_threads(edit, threads)
+
+	def handle_threads(self, edit, threads, offset=0, i=0, dir=1):
+		next_threads = []
+		for thread in threads:
+			if thread.is_alive():
+				next_threads.append(thread)
+				continue
+			if thread.result == False:
+				continue
+			offset = self.replace(edit, thread, offset)
+		threads = next_threads
+
+
+
+		if len(threads):
+			# This animates a little activity indicator in the status area
+			before = i % 8
+			after = (7) - before
+			if not after:
+				dir = -1
+			if not before:
+				dir = 1
+			i += dir
+			self.view.set_status('prefixr', 'Prefixr [%s=%s]' % (' ' * before, ' ' * after))
+
+			sublime.set_timeout(lambda: self.handle_threads(edit, threads, offset, i, dir), 100)
+    	return
