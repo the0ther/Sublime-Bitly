bd2fdae483a23ceb8c9714e23319b796111c1e21
Randy L <randay@gmail.com>
2013-11-02 17:43:47 -0400

    this is wip

--- Bitly.py
+++ Bitly.py
@@ -5,8 +5,8 @@ import json
 import urlparse
 import logging as logger
 
-import BitlyShorten as BitlyShorten
-import BitlyExpand as BitlyExpand
+from BitlyShorten import BitlyShorten 
+from BitlyExpand import BitlyExpand
 
 # The inspiration for this is the following Textmate plugin:
 # http://bit.ly/18MdHDI
@@ -53,7 +53,8 @@ class BitlyShortenCommand(sublime_plugin.TextCommand):
         continue
       if thread.result == False:
         continue
-      offset = self.replace(edit, thread, offset)
+      # offset = self.replace(edit, thread, offset)
+
     threads = next_threads
 
     if len(threads):
@@ -100,65 +101,55 @@ class BitlyShortenCommand(sublime_plugin.TextCommand):
 
 # This expands a bitly URL
 class BitlyExpandCommand(sublime_plugin.WindowCommand):
-  def run(self, edit):
+  def run(self):
     self.settings = sublime.load_settings("Bitly.sublime-settings")
     logger.debug("inside BitlyExpandCommand.run()")
 
-    window = self.window
-    view = window.active_view()
-    sel = view.sel()
+    self.view = self.window.active_view()
+    self.selections = self.view.sel()
+    # print self.sel
+    threads = []
 
-    region1 = sel[0]
-    selectionText = view.substr(region1)
-    print(selectionText)
+    for selection in self.selections:
+      print selection
+      string = self.view.substr(selection)
+      print string
+      thread = BitlyExpand(selection, string, 15, self.settings.get("api_login"), self.settings.get("api_key"))
+      threads.append(thread)
+      thread.start()
+    edit = self.view.begin_edit('bitly')
+    self.handle_threads(edit, threads)
 
+  def handle_threads(self, edit, threads, offset=0, i=0, dir=1):
+    next_threads = []
+    for thread in threads:
+      if thread.is_alive():
+        next_threads.append(thread)
+        continue
+      if thread.result == False:
+        continue
+      # offset = self.replace(edit, thread, offset)
+    threads = next_threads
 
-    # self.urls = self.view.find_all(r'\b(([\w-]+://?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^\s]|/)))\b')
-    # logger.debug(self.urls)
-    # threads = []
-    # for url in self.urls:
-    #   logger.debug(url)
-    #   string = self.view.substr(url)
-    #   string = self.strip_parens(string)
-    #   logger.debug("string stripped: " + string)
-    #   logger.debug(self.settings.get("api_login"))
-    #   logger.debug("user " + str(self.view.settings().get("api_login")))
-    #   thread = BitlyExpand(url, string, 15, self.settings.get("api_login"), self.settings.get("api_key"))
-    #   threads.append(thread)
-    #   thread.start()
-    # edit = self.view.begin_edit('bitly')
-    # self.handle_threads(edit, threads)
+    if len(threads):
+      # This animates a little activity indicator in the status area
+      before = i % 8
+      after = (7) - before
+      if not after:
+        dir = -1
+      if not before:
+        dir = 1
+      i += dir
+      self.view.set_status('bitly', 'Bitly [%s=%s]' % (' ' * before, ' ' * after))
 
-  # def handle_threads(self, edit, threads, offset=0, i=0, dir=1):
-  #   next_threads = []
-  #   for thread in threads:
-  #     if thread.is_alive():
-  #       next_threads.append(thread)
-  #       continue
-  #     if thread.result == False:
-  #       continue
-  #     offset = self.replace(edit, thread, offset)
-  #   threads = next_threads
+      sublime.set_timeout(lambda: self.handle_threads(edit, threads, offset, i, dir), 100)
+      return
 
-  #   if len(threads):
-  #     # This animates a little activity indicator in the status area
-  #     before = i % 8
-  #     after = (7) - before
-  #     if not after:
-  #       dir = -1
-  #     if not before:
-  #       dir = 1
-  #     i += dir
-  #     self.view.set_status('bitly', 'Bitly [%s=%s]' % (' ' * before, ' ' * after))
+    self.view.end_edit(edit)
 
-  #     sublime.set_timeout(lambda: self.handle_threads(edit, threads, offset, i, dir), 100)
-  #     return
-
-  #   self.view.end_edit(edit)
-
-  #   self.view.erase_status('bitly')
-  #   matches = len(self.urls)
-  #   sublime.status_message('Bitly successfully run on %s selection%s' % (matches, '' if matches == 1 else 's'))
+    self.view.erase_status('bitly')
+    matches = len(self.selections)
+    sublime.status_message('Bitly successfully run on %s selection%s' % (matches, '' if matches == 1 else 's'))
 
   # def replace(self, edit, thread, offset):
   #   sel = thread.sel
@@ -170,7 +161,7 @@ class BitlyExpandCommand(sublime_plugin.WindowCommand):
   #     # print 'offset: %s' % offset
   #     sel = sublime.Region(sel.begin() + offset, sel.end() + offset)
 
-  #   self.view.replace(edit, sel, result)
+  #   # self.view.replace(edit, sel, result)
 
   #   # We add the end of the new text to the selection
   #   return offset + len(result) - len(original)
--- BitlyExpand.py
+++ BitlyExpand.py
@@ -6,7 +6,7 @@ import json
 import logging as logger
 
 class BitlyExpand(threading.Thread):
-  def __init__(self, sel, string, timeout, user, key):
+  def __init__(self, string, timeout, user, key):
     self.sel = sel
     self.original = string
     self.timeout = timeout
@@ -18,13 +18,16 @@ class BitlyExpand(threading.Thread):
   def run(self):
     try:
       encUrl = urllib.urlencode({"shortUrl": self.original})
+      # print encUrl
       reqUrl = 'http://api.bitly.com/v3/expand?login=' + self.user + '&apiKey=' + self.key + '&' + encUrl
+      print "reqUrl: " + reqUrl
       request = urllib2.Request(reqUrl, headers={"User-Agent": "Sublime Bitly"})
       http_file = urllib2.urlopen(request, timeout=self.timeout)
       bitlyRes = http_file.read()
       bitlyObj = json.loads(bitlyRes)
-      self.result = bitlyObj['data']['long_url']
-      logger.debug("bitly encoded url: " + self.result)
+      print bitlyObj
+      # self.result = bitlyObj['data']['long_url']
+      # print "bitly encoded url: " + self.result
       return
     except (urllib2.HTTPError) as (e):
       err = '%s: HTTP error %s contacting API' % (__name__, str(e.code))
